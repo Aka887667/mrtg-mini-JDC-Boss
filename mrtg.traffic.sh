8a5601943f4060803b84c0ff1e1d99844d9cc520
gaspard <gaspard@gmail.com>
2018-01-28 14:43:04 +0100

    pi zero w has wlan by default

--- mrtg.traffic.sh
+++ mrtg.traffic.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-netstat -i | grep -e eth0 | awk '{print $4}' # RX
-netstat -i | grep -e eth0 | awk '{print $8}' # TX
+netstat -i | grep -E 'eth0|wlan0' | awk '{print $4}' # RX
+netstat -i | grep -E 'eth0|wlan0' | awk '{print $8}' # TX
 echo 0
 echo 0
-
