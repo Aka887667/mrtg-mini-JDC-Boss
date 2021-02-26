ca9c6a3258f32fc1fb67c2c5aeca487db9d28fd4
Gaspard Beernaert <gaspard@gmail.com>
2021-02-26 09:28:42 +0100

    fix: new mrtg

--- mrtg.mem.sh
+++ mrtg.mem.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #mem used by apps and services = used - buffes - caches
-free -k | grep Mem | awk '{print ($3-$6-$7)}'
+free -k | grep Mem | awk '{print ($2-$7)}'
 # swap
 free -k | grep Swap | awk '{print $3}'
 echo 0
