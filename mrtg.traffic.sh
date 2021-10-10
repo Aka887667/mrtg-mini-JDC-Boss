43565ae80e496db2d5c64e0033198e5a01e44b2e
Aka887667 <92236736+Aka887667@users.noreply.github.com>
2021-10-10 14:55:02 +0800

    Update mrtg.traffic.sh

--- mrtg.traffic.sh
+++ mrtg.traffic.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 netstat -i | grep -E 'eth0|wlan0' | awk '{print $3}' # RX
-netstat -i | grep -E 'eth0|wlan0' | awk '{print $7}' # TX
+#netstat -i | grep -E 'eth0|wlan0' | awk '{print $4}' # RX
+#netstat -i | grep -E 'eth0|wlan0' | awk '{print $7}' # TX
+netstat -i | grep -E 'eth0|wlan0' | awk '{print $8}' # TX
 echo 0
 echo 0
